b0707b4b71e01aa234797b685f0eb2bdd6ecca3f
Ferdy Adriantama <ferdypbg@gmail.com>
2025-04-21 04:09:46 +0700

    feat: complete slicing page AudioPlayer

--- lib/app/modules/audio_player/controllers/audio_player_controller.dart
+++ lib/app/modules/audio_player/controllers/audio_player_controller.dart
@@ -2,4 +2,5 @@ import 'package:get/get.dart';
 
 class AudioPlayerController extends GetxController {
   RxBool isPaused = false.obs;
+  RxDouble position = 0.0.obs;
 }
--- lib/app/modules/audio_player/views/audio_player_view.dart
+++ lib/app/modules/audio_player/views/audio_player_view.dart
@@ -63,32 +63,177 @@ class AudioPlayerView extends GetView<AudioPlayerController> {
                       ),
                     ),
                     Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      child: Column(
                         children: [
-                          Icon(
-                            Icons.share_outlined,
-                            color: Colors.white,
-                            size: 30,
+                          SizedBox(height: 16),
+                          Row(
+                            spacing: 16,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  spacing: 8,
+                                  children: [
+                                    Text(
+                                      'How to build intentional UX in an era of persuasive technology',
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: Fonts.mainText.copyWith(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600,
+                                        height: 1,
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Emily',
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Fonts.mainText.copyWith(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w500,
+                                            height: 1.5,
+                                          ),
+                                        ),
+                                        Text('  •  ', style: Fonts.mainText),
+                                        Text(
+                                          'UI/UX Designer',
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Fonts.mainText.copyWith(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w300,
+                                            height: 1.5,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Icon(
+                                Icons.headphones,
+                                color: Color(0xFFFFE485),
+                                size: 22,
+                              ),
+                              Icon(
+                                Icons.play_arrow_rounded,
+                                color: Colors.white,
+                              ),
+                            ],
                           ),
-                          Icon(
-                            Icons.skip_previous,
-                            color: Colors.white,
-                            size: 58,
+                          SizedBox(height: 16),
+                          Row(
+                            spacing: 8,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF222326),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Row(
+                                  spacing: 8,
+                                  children: [
+                                    Icon(
+                                      Icons.people,
+                                      color: Colors.white,
+                                      size: 16,
+                                    ),
+                                    Text(
+                                      'Soft Skill',
+                                      style: Fonts.mainText.copyWith(
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  'Aug 4 • in English',
+                                  textAlign: TextAlign.end,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: Fonts.mainText.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                          Icon(
-                            controller.isPaused.isTrue
-                                ? Icons.pause_circle_filled
-                                : Icons.play_circle_fill,
-                            color: Colors.white,
-                            size: 58,
+                          SizedBox(height: 16),
+                          Obx(() {
+                            return Slider(
+                              value: controller.position.value,
+                              onChanged: (value) {
+                                controller.position.value = value;
+                              },
+                              activeColor: Colors.white,
+                              inactiveColor: Color(0xFF534F4F),
+                              padding: EdgeInsets.all(0),
+                              allowedInteraction: SliderInteraction.tapAndSlide,
+                            );
+                          }),
+                          SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '1:23',
+                                style: Fonts.mainText.copyWith(
+                                  color: Color(0xFFB8B7B5),
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                '3:36',
+                                style: Fonts.mainText.copyWith(
+                                  color: Color(0xFFB8B7B5),
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
                           ),
-                          Icon(Icons.skip_next, color: Colors.white, size: 58),
-                          Icon(
-                            Icons.bookmark_border_rounded,
-                            color: Colors.white,
-                            size: 30,
-                          ),
+                          Spacer(),
+                          Obx(() {
+                            return Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Icon(
+                                  Icons.share_outlined,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                                Icon(
+                                  Icons.skip_previous_rounded,
+                                  color: Colors.white,
+                                  size: 58,
+                                ),
+                                GestureDetector(
+                                  onTap: () => controller.isPaused.toggle(),
+                                  child: Icon(
+                                    controller.isPaused.isTrue
+                                        ? Icons.pause_circle_filled_rounded
+                                        : Icons.play_circle_fill_rounded,
+                                    color: Colors.white,
+                                    size: 58,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.skip_next_rounded,
+                                  color: Colors.white,
+                                  size: 58,
+                                ),
+                                Icon(
+                                  Icons.bookmark_border_rounded,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ],
+                            );
+                          }),
+                          SizedBox(height: 16),
                         ],
                       ),
                     ),
