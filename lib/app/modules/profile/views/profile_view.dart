42d1c45e1784bf5a8d48ba7f437ffb75de2d6eac
Ferdy Adriantama <ferdypbg@gmail.com>
2025-04-21 03:07:21 +0700

    feat: dynamic profile page when logged in or vice versa
    menampilkan nama user pada profile page ketika user login; button
    dinamis berdasarkan status login;

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:talent_insider_learning/app/core/themes/fonts.dart';
 import 'package:talent_insider_learning/app/core/themes/style.dart';
 import 'package:talent_insider_learning/app/global_widgets/secondary_button.dart';
+import 'package:talent_insider_learning/app/routes/app_pages.dart';
 
 import '../controllers/profile_controller.dart';
 
@@ -12,13 +14,53 @@ class ProfileView extends GetView<ProfileController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Style.background01,
-      body: Center(
-        child: SecondaryButton(
-          title: 'Logout',
-          onPressed: () async {
-            await controller.authController.logout();
-          },
-        ),
+      body: SafeArea(
+        child: Obx(() {
+          return Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                if (controller.authController.user.value != null) ...[
+                  Row(
+                    children: [
+                      Text(
+                        'Hi, ',
+                        style: Fonts.mainText.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        '${controller.authController.user.value?.firstName} ${controller.authController.user.value?.lastName}',
+                        style: Fonts.mainText.copyWith(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+                Expanded(
+                  child: Center(
+                    child: Visibility(
+                      visible: controller.authController.user.value != null,
+                      replacement: SecondaryButton(
+                        title: 'Sign in',
+                        onPressed: () => Get.offAllNamed(Routes.LOGIN),
+                      ),
+                      child: SecondaryButton(
+                        title: 'Logout',
+                        onPressed: () async {
+                          await controller.authController.logout();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
